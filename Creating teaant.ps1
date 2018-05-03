3eab1e360157b919570cb54aa041048fa5517028
SrinivasGudibanda <38935925+SrinivasGudibanda@users.noreply.github.com>
2018-05-03 12:57:45 +0530

    Add files via upload

---
+++ Creating teaant.ps1
@@ -0,0 +1,16 @@
+﻿Login-AzureRmAccount
+
+cd D:\LatestRDmi\LatestRDmi\PowershellModules
+
+Import-Module .\Microsoft.RdInfra.RdPowershell.dll
+
+$password = "keepcalm@123" | ConvertTo-SecureString -asPlainText -Force
+$username = "deepak.jena@peopletechcsp.onmicrosoft.com" 
+
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+Set-RdsContext -DeploymentUrl 'http://rdbroker-2t22o32gtse56.azurewebsites.net' -Credential $credential
+
+Get-RdsTenant
+
+
